434b6bf58c8b74d50d3de45149ee5afacf86de10
Mahesh-VM <maheshvm218@gmail.com>
2021-11-24 13:25:45 +0530

    Practise for react components using typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import AddToList from './components/addtolist';
+import List from './components/list'
+
+export interface IState {
+  people: {
+    name: string,
+    age: number,
+    url: string,
+    note?: string
+  }[]
+}
 
 function App() {
+  const [people, setPeople] = useState<IState['people']>([
+    {
+      name: "M S Dhoni",
+      age: 40,
+      url: "https://hwnews.in/wp-content/uploads/2019/04/MS-Dhoni.jpg",
+      note: "Retired form BCCI."
+    },
+    {
+      name: "M S Dhoni",
+      age: 40,
+      url: "https://hwnews.in/wp-content/uploads/2019/04/MS-Dhoni.jpg",
+      note: "Retired form BCCI."
+    }
+  ]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited for party.</h1>
+      <List people={people}/>
+      <AddToList people={people} setPeople={setPeople}/>
     </div>
   );
 }
